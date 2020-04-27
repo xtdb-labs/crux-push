70f7f7d466715ee189401d2202fc6f15a8d49bb2
Tom Taylor <tmt@juxt.pro>
2020-04-27 15:48:23 +0100

    Translate example comment into a test

--- example/work_bookings/core.clj
+++ example/work_bookings/core.clj
@@ -1,6 +1,8 @@
 (ns work-bookings.core
+  "Contains an example API endpoint for a internal work holiday booking system"
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [clojure.test :as t]
             [clojure.tools.logging :as log]
             [crux.api :as crux]
             [crux.push :as cx-push]
@@ -8,37 +10,6 @@
             [juxt.jinx-alpha.resolve :as jinxresolv]
             [schema.core :as s]
             [yada.yada :as yada]))
-
-(comment
-  (with-open [node
-              (crux/start-node {:crux.node/topology '[crux.standalone/topology]})]
-    (let [ingestor
-          (cx-push/create-ingestor
-            {:schema
-             (edn/read-string (slurp (io/resource "work_bookings/schema.edn")))
-             :crux node})
-          ;; Add admin user and take uuid
-          mal (ingestor {"code" "mal"
-                         "name" "Malcolm"})
-          ;; Make mal a manager
-          mal-manager (ingestor {"employeeInfo" (str (get mal :crux.db/id))})
-          ;; Give mal some employees
-          tmt (ingestor {"code" "tmt"
-                         "name" "Tom"
-                         "manager" (str (get mal-manager :crux.db/id))})
-          dan (ingestor {"code" "dan"
-                         "name" "Dan"
-                         "manager" (str (get mal-manager :crux.db/id))})
-          ;; Make jon and employees in one go
-          jon-manager (ingestor {"employeeInfo" "0cd75dbf-03fa-4af5-9750-35d6a18ffe4e"
-                                 "employees" [{"code" "hjy", "name" "Hugo"}
-                                              {"code" "joa", "name" "Johanna"}
-                                              {"code" "jon", "name" "Jon", "crux:id" "0cd75dbf-03fa-4af5-9750-35d6a18ffe4e"}]})]
-      (crux/sync node)
-      (crux/q (crux/db node)
-              '{:find [e]
-                :where [[e :crux.db/id e]]
-                :full-results? true}))))
 
 (defn booking-resource [node]
   (let [ingestor
@@ -50,7 +21,7 @@
        {:get
         {:produces [{:media-type "text/plain"}]
          :response
-         (fn [ctx]
+         (fn [_]
            (let [alerts (crux/q
                           (crux/db node)
                           '{:find [e]
@@ -86,23 +57,73 @@
                   {:media-type "application/edn"}]
        :produces [{:media-type "application/json"}]
        :parameters {:body s/Any}
-       :response
-       (fn [ctx]
-         (let [body (get-in ctx [:parameters :body])]
-           {:status 200
-            :result (crux/q (crux/db node) body)}))}}}))
+       :response (fn [ctx]
+                   (let [body (get-in ctx [:parameters :body])]
+                     {:status 200
+                      :result (crux/q (crux/db node) body)}))}}}))
 
 (defn svr [node]
-  (yada/listener ["/" [["ingest"
-                        (#'work-bookings.core/booking-resource node)]
-                       ["query"
-                        (#'work-bookings.core/query-resource node)]]]
-                 {:port 3000}))
+  (let [s (yada/listener ["/" [["ingest"
+                                (#'work-bookings.core/booking-resource node)]
+                               ["query"
+                                (#'work-bookings.core/query-resource node)]]]
+                         {:port 3000})]
+    (reify java.io.Closeable
+      (close [_]
+        (.close node)
+        ((:close s))))))
 
 (comment
+  ;; start server
   (def node (crux/start-node {:crux.node/topology '[crux.standalone/topology]}))
   (def ss (svr node))
-  ((:close ss)))
+  (.close ss))
 
-(crux/q (crux/db node) '{:find [e]
-                         :where [[e "manager" m]]})
+(t/deftest holiday-system-test
+  (with-open [node (crux/start-node
+                     {:crux.node/topology
+                      '[crux.standalone/topology]})]
+    (let [ingestor
+          (cx-push/create-ingestor
+            {:schema
+             (edn/read-string
+               (slurp
+                 (io/resource "work_bookings/schema.edn")))
+             :crux node})
+          ;; Add admin user and take uuid
+          mal (ingestor {"code" "mal"
+                         "name" "Malcolm"})
+          ;; Make mal a manager
+          mal-manager (ingestor {"employeeInfo" (str (get mal :crux.db/id))})
+          ;; Give mal some employees
+          tmt (ingestor {"code" "tmt"
+                         "name" "Tom"
+                         "manager" (str (get mal-manager :crux.db/id))})
+          dan (ingestor {"code" "dan"
+                         "name" "Dan"
+                         "manager" (str (get mal-manager :crux.db/id))})
+          jon (ingestor
+                {"code" "jon", "name" "Jon"})
+          ;; Make jon and employees in one go
+          jon-manager (ingestor {"employeeInfo" (str (get jon :crux.db/id))
+                                 "employees" [{"code" "hjy", "name" "Hugo"}
+                                              {"code" "joa", "name" "Johanna"}]})]
+      (crux/sync node)
+
+      ;; Employees are tied to their managers
+      (t/is #{["Dan"] ["Tom"]}
+            (crux/q (crux/db node)
+                    '{:find [n]
+                      :where [[m :crux.push/type "manager"]
+                              [m :employeeInfo ei]
+                              [ei :code "mal"]
+                              [e :manager m]
+                              [e :name n]]}))
+      (t/is #{["Hugo"] ["Johanna"]}
+            (crux/q (crux/db node)
+                    '{:find [n]
+                      :where [[m :crux.push/type "manager"]
+                              [m :employeeInfo ei]
+                              [ei :code "jon"]
+                              [e :manager m]
+                              [e :name n]]})))))
